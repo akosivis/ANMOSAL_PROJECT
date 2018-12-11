8d45847f07b48962bc851f6dc0b4fcd65be9df4d
viselvis <abelvis@up.edu.ph>
2018-12-11 16:51:30 +0800

    Chat UI in GamePanels (MainPAnelServer.java)

--- MainPanelServer.java
+++ MainPanelServer.java
@@ -15,13 +15,10 @@ public class MainPanelServer extends JFrame {
 		System.out.println(address);
 		g_panel =  new GamePanel(name, address, 4443, player, false, team);
 		
-<<<<<<< HEAD
 		g_server = new GameServer(player, port);
 		chat_server = new ChatForServer();
-=======
 		
 		g_server = new GameServer(player, port, address);
->>>>>>> 960be86c1aa635527fd8553feda00086c7b88cc2
 		new Thread(g_server).start();
 		
 		this.setLayout(new BorderLayout());		
@@ -30,12 +27,8 @@ public class MainPanelServer extends JFrame {
 		
 		Thread t2 = new Thread(g_panel);
 		t2.start();
-<<<<<<< HEAD
-
-=======
 		
 		this.setTitle("Conquer! Connecting to: " + address);
->>>>>>> 960be86c1aa635527fd8553feda00086c7b88cc2
 		setResizable(false);
 		setVisible(true);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
